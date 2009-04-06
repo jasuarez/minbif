0f51381063ece488ca329dcad3e4a16746ba0ce0
Romain Bignon <romain@peerfuse.org>
2009-04-06 23:19:57 +0200

    added the CallBack class

---
+++ src/callback.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright(C) 2009 Romain Bignon
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "callback.h"
+#include "log.h"
+
+void g_callback(void* data, gint, PurpleInputCondition)
+{
+	_CallBack* cb = static_cast<_CallBack*>(data);
+
+	if(!data)
+	{
+		b_log[W_ERR] << "g_callback() handled with non CallBack instance";
+		return;
+	}
+
+	cb->run();
+}
---
+++ src/callback.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright(C) 2009 Romain Bignon
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+#include <libpurple/purple.h>
+
+class _CallBack
+{
+public:
+	virtual ~_CallBack() {}
+	virtual void run() = 0;
+};
+
+template<typename T>
+class CallBack : public _CallBack
+{
+public:
+
+        typedef void (T::*TFunc) (void*);
+
+        CallBack(T* _obj, TFunc _func, void* _data = 0) : obj(_obj), func(_func), data(_data) {}
+
+        virtual void run()
+        {
+                (obj->*func) (data);
+        }
+
+private:
+        T* obj;
+        TFunc func;
+        void *data;
+};
+
+void g_callback(void* data, gint, PurpleInputCondition);
+
+#endif /* CALLBACK_H */
