0baf56d9213ee39affe53dcea61fa514affe7933
Romain Bignon <romain@peerfuse.org>
2009-05-10 00:23:00 +0200

    do not show purple warnings anymore

--- src/im/purple.cpp
+++ src/im/purple.cpp
@@ -47,7 +47,7 @@ void Purple::debug(PurpleDebugLevel level, const char *category, const char *arg
 {
 	switch(level)
 	{
-		case PURPLE_DEBUG_WARNING:
+		//case PURPLE_DEBUG_WARNING:
 		case PURPLE_DEBUG_ERROR:
 		case PURPLE_DEBUG_FATAL:
 			b_log[W_WARNING] << "[" << category << "] " << args;
