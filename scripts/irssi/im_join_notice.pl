b10b8ea66f1ac3a40dd69a563762e159ceb1e5f7
Sebastien Delafond <sdelafond@gmail.com>
2009-08-25 16:31:01 +0200

    irssi scripts to handle typing notifications. - work in both bitlbee and minbif - externalize common code in im_utils.pm - code cleanup in both im_{join,typing}_notice.pl

--- scripts/irssi/im_join_notice.pl
+++ scripts/irssi/im_join_notice.pl
@@ -1,5 +1,8 @@
 #CHANGELOG:
 #
+# 25-08-2009 (2.1)
+# * externalize some utilities in im_utils.pm
+#
 #17-08-2009:
 #modified so it works with minbif too
 #
@@ -7,51 +10,39 @@
 #it gives a join message in a query if a user joins &bitlbee and you hve a query open with that person.
 #
 #/statusbar window add join_notice
-#use Data::Dumper;
 
 use strict;
 use Irssi::TextUI;
-#use Irssi::Themes;
+require "im_utils.pm";
 
 use vars qw($VERSION %IRSSI);
 
-$VERSION = '2.0';
+$VERSION = '2.1';
 %IRSSI = (
-    authors     => 'Tijmen "timing" Ruizendaal, Sebastien Delafond',
+    authors     => 'Tijmen "timing" Ruizendaal, Sébastien Delafond',
     contact     => 'tijmen.ruizendaal@gmail.com, seb@debian.org',
     name        => 'IM_join_notice',
     description => '	1. Adds an item to the status bar wich shows [joined: <nicks>] when someone is joining on IM.
     			2. Shows join messages in the query.',
     license => 'GPLv2',
     url         => 'http://symlink.me/repositories/show/minbif/scripts/irssi',
-    changed => '2009-08-17',
+    changed => '2009-08-25',
 );
 
 my %online;
 my %tag;
-my @control_channels = ();
+my @control_channels;
 
-#my $debug_window = Irssi::window_find_item("(init)");
+&set_local_channels();
 
-get_channels();
-
-sub get_channels {
-  my @channels = Irssi::channels();
-  foreach my $channel (@channels) {
-    my $name = $channel->{name};
-    my $topic = $channel->{topic};
-    my $type = $channel->{server}->{tag};
-    if (($name =~ m/^&/) && (($topic eq "Welcome to the control channel. Type \x02help\x02 for help information.") || ($type eq "minbif"))) {
-#      $debug_window->print("$name, $type\n");
-      push(@control_channels, ($name, $type));
-    }
-  }
+sub set_local_channels {
+  @control_channels = @{&ImUtils::get_channels()};
 }
 
 sub event_join {
   my ($server, $channel, $nick, $address) = @_;
-#  $debug_window->print("$server, $channel, $nick, $address\n");
-  if (grep {$_ eq ($channel, $server->{tag})} @control_channels) {
+#  print("Looking at join for: $server, $channel, $nick, $address");
+  if (ImUtils::is_im($server, $channel, \@control_channels)) {
     $online{$nick} = 1;
     Irssi::timeout_remove($tag{$nick});
     $tag{$nick} = Irssi::timeout_add(7000, 'empty', $nick);
@@ -83,8 +74,9 @@ sub empty{
   Irssi::statusbar_items_redraw('join_notice');
 }
 
-Irssi::signal_add_last('channel sync', 'get_channels');
 Irssi::signal_add("event join", "event_join");
+
+Irssi::signal_add_last('channel sync', "set_local_channels");
 
 Irssi::statusbar_item_register('join_notice', undef, 'join_notice');
 Irssi::statusbars_recreate_items();
