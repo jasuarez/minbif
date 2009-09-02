ac54d83ccd5bed7968c6d37bf9a55e6bd5cdf810
Romain Bignon <romain@peerfuse.org>
2009-09-02 15:23:46 +0200

    removed useless mutex.* files, updated copyrights

--- src/log.h
+++ src/log.h
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2008-2009 Laurent Defert, Romain Bignon
+ * Copyright(C) 2009 Laurent Defert, Romain Bignon
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
