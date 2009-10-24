7061e9091e4376dde3a206570067297c411916e0
Romain Bignon <romain@peerfuse.org>
2009-10-24 18:17:17 +0200

    minbif 1.0RC released

--- src/core/version.h
+++ src/core/version.h
@@ -24,7 +24,7 @@
 #define MINBIF_VERSION_MAJOR   "1"
 #define MINBIF_VERSION_MINOR   "0"
 #define MINBIF_VERSION_PATCH   ""
-#define MINBIF_VERSION_EXTRA   "beta2"
+#define MINBIF_VERSION_EXTRA   "RC"
 
 #ifdef USE_CACA
 #define MINBIF_SUPPORTS_CACA "-caca"
