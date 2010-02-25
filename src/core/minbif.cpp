2c7891fed79b8edef6dfdb4eaa63ba9a4f1a48bf
Romain Bignon <romain@peerfuse.org>
2010-02-25 18:07:14 +0100

    typo in /INFO

--- src/core/minbif.cpp
+++ src/core/minbif.cpp
@@ -41,7 +41,7 @@ const char* infotxt[] = {
 	"Copyright(C) 2009-2010 Romain Bignon",
 	"This program is free software; you can redistribute it and/or modify",
 	"it under the terms of the GNU General Public License as published by",
-	"the Free Software Foundation; either version 2 of the License.",
+	"the Free Software Foundation, version 2 of the License.",
 	NULL
 };
 
