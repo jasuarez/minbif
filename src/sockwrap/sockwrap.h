498851c865a9a553345c2602f049b3fbfd2986c5
Frank Steinborn <steinex@nognu.de>
2010-04-09 14:08:10 +0200

    Fix compilation on FreeBSD, spotted by Quentin Stievenart

--- src/sockwrap/sockwrap.h
+++ src/sockwrap/sockwrap.h
@@ -24,6 +24,11 @@
 #include "core/config.h"
 #include "core/callback.h"
 
+#if defined(__FreeBSD__) || defined(__FreeBSD)
+#include <sys/types.h>
+#include <sys/socket.h>
+#endif
+
 #ifndef PF_SOCKWRAP_H
 #define PF_SOCKWRAP_H
 
