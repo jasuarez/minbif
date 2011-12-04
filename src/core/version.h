28675c386d490c384dcf61b7f1c5875c934aec1d
Romain Bignon <romain@peerfuse.org>
2011-12-04 15:24:51 +0100

    Minbif 1.0.5 (Slutty Sheraz) released

--- src/core/version.h
+++ src/core/version.h
@@ -24,8 +24,8 @@
 #define MINBIF_DEV_WEBSITE  "http://symlink.me/projects/show/minbif"
 
 #define MINBIF_VERSION_MAJOR   "1"
-#define MINBIF_VERSION_MINOR   "1"
-#define MINBIF_VERSION_PATCH   "-dev"
+#define MINBIF_VERSION_MINOR   "0.5"
+#define MINBIF_VERSION_PATCH   ""
 #define MINBIF_VERSION_EXTRA   ""
 
 #ifdef HAVE_CACA
